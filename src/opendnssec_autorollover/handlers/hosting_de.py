b86cb0e7e4a6bef5818818b9e669cb2b32dd861c
Julian Brost <julian@0x4a42.net>
2018-01-12 20:41:51 +0100

    Rewrite hosting.de handler to use new API methods and clean up code
    Actually these methods are not documented yet, but hosting.de support
    was friendly and told me they already exist. :)

--- src/opendnssec_autorollover/handlers/hosting_de.py
+++ src/opendnssec_autorollover/handlers/hosting_de.py
@@ -1,81 +1,95 @@
 import logging
 import requests
 
-API_DOMAIN_INFO = 'https://secure.hosting.de/api/domain/v1/json/domainInfo'
-API_DOMAIN_UPDATE = 'https://secure.hosting.de/api/domain/v1/json/domainUpdate'
+from opendnssec_autorollover.handlers import HandlerBase
+
+API_BASE = 'https://secure.hosting.de/api'
+
+API_DOMAIN_DNSSEC_KEYS_LIST = 'domain/v1/json/dnsSecKeysList'
+API_DOMAIN_DNSSEC_KEYS_MODIFY = 'domain/v1/json/dnsSecKeyModify'
+
+def res_check(res):
+    return res
+
+def dnskey_to_api(key):
+    flags, alg, pub = key
+    return {
+        'keyData': {
+            'flags': flags,
+            'protocol': 3,
+            'algorithm': alg,
+            'publicKey': pub,
+        }
+    }
+
+def dnskey_from_api(key):
+    flags = key['keyData']['flags']
+    alg = key['keyData']['algorithm']
+    pub = key['keyData']['publicKey']
+    return (flags, alg, pub)
 
 class HostingDeHandler(HandlerBase):
 
-    def res_check(self, res):
+    def __init__(self, zone, config):
+        super(HostingDeHandler, self).__init__(zone, config)
+        self.domain = zone.rstrip('.')
+
+    @property
+    def api_base(self):
+        return self.config.get('api_base', API_BASE)
+
+    def api_request(self, method, req=None):
+        url = '{}/{}'.format(self.api_base, method)
+        if req is None:
+            req = dict()
+        if not 'authToken' in req:
+            req = dict(req, authToken=self.config['auth_token'])
+        res = res_check(requests.post(url, json=req))
         assert res.status_code == 200
         res = res.json()
         for w in res.get('warnings', []):
             logging.warning('hosting.de API: %s', w)
         for e in res.get('errors', []):
             logging.error('hosting.de API: %s', e)
-        assert res['status'] in ['success', 'pending']
+        if res['status'] not in ['success', 'pending']:
+            raise Exception('hosting.de API returned status "{}"'.format(res['status']), res)
         return res
 
-    def get_domain(self, config, domain):
-        req = {
-            'authToken': config['auth_token'],
-            'domainName': domain,
-        }
-        res = self.res_check(requests.post(API_DOMAIN_INFO, json=req))
-        domain_obj = res['response']
-        return domain_obj
+    def get_dnskeys(self):
+        req = {'domainName': self.domain}
+        res = self.api_request(API_DOMAIN_DNSSEC_KEYS_LIST, req)
+        return set(dnskey_from_api(k) for k in res['responses'])
+
+    def update_dnskeys(self, add=None, remove=None):
+        if not (add or remove):
+            logging.debug('%s: nothing to update, skipping', self.domain)
+            return
+
+        req = {'domainName': self.domain}
+        if add:
+            req['add'] = [dnskey_to_api(k) for k in add]
+        if remove:
+            req['remove'] = [dnskey_to_api(k) for k in remove]
+        return self.api_request(API_DOMAIN_DNSSEC_KEYS_MODIFY, req)
 
     def make_key_delta(self, current_dnskeys, changes):
         dnskey_add = []
         dnskey_remove = []
 
-        current = set() # (flags, alg, pubkey)
-        for key in current_dnskeys:
-            flags = key['keyData']['flags']
-            alg = key['keyData']['algorithm']
-            pub = key['keyData']['publicKey']
-            current.add((flags, alg, pub))
-
         for state, key in changes:
-            flags, alg, pub = key
-
-            if state == 'ready' and key not in current:
+            if state == 'ready' and key not in current_dnskeys:
                 logging.debug('add: %s', key)
-                dnskey_add.append({'keyData': {'flags': flags, 'protocol': 3, 'algorithm': alg, 'publicKey': pub}})
-
-            if state == 'retire' and key in current:
+                dnskey_add.append(key)
+            if state == 'retire' and key in current_dnskeys:
                 logging.debug('remove: %s', key)
-                dnskey_remove.append({'keyData': {'flags': flags, 'protocol': 3, 'algorithm': alg, 'publicKey': pub}})
+                dnskey_remove.append(key)
 
         return dnskey_add, dnskey_remove
 
-    def update_domain(self, config, domain_obj, dnskey_add, dnskey_remove):
-        change = False
-
-        req = {
-            'authToken': config['auth_token'],
-            'domain': domain_obj,
-        }
-
-        if dnskey_add:
-            req['dnsSecKeyAdd'] = dnskey_add
-            change = True
-
-        if dnskey_remove:
-            req['dnsSecKeyRemove'] = dnskey_remove
-            change = True
-
-        if change:
-            res = self.res_check(requests.post(API_DOMAIN_UPDATE, json=req))
-            logging.debug('updated domain: %s', res)
-
-    def handle(self, zone, config, changes):
-        domain = zone.rstrip('.')
-        domain_obj = self.get_domain(config, domain)
-        current_dnskeys = domain_obj['dnsSecEntries']
-        dnskey_add, dnskey_remove = self.make_key_delta(current_dnskeys, changes)
-        self.update_domain(config, domain_obj, dnskey_add, dnskey_remove)
-
-    # TODO: remove
     def run(self, changes):
-        self.handle(self.zone, self.config, changes)
+        current_dnskeys = self.get_dnskeys()
+        logging.debug('%s: current: %s', self.domain, current_dnskeys)
+        add, remove = self.make_key_delta(current_dnskeys, changes)
+        logging.debug('%s: add: %s', self.domain, add)
+        logging.debug('%s: remove: %s', self.domain, remove)
+        self.update_dnskeys(add=add, remove=remove)
